7d9226dc8a7d8d871b0a9e22d47851d4718f4654
Tianyu Cheng <tianyu.cheng@utexas.edu>
2025-06-23 21:49:10 -0500

    [CMake] fix small things related to packaging on Windows again

--- Scripts/install.bat
+++ Scripts/install.bat
@@ -7,19 +7,21 @@ echo ### Configure CMake...
 echo ######################
 cmake -S %GIT_ROOT% -B %GIT_ROOT%\Scratch -A x64 --preset MSVC
 
-echo ###########################
+echo #############################
 echo ### Build debug binaries...
-echo ###########################
+echo #############################
 cmake --build %GIT_ROOT%\Scratch --config Debug
 
 echo #############################
 echo ### Build release binaries...
 echo #############################
 cmake --build %GIT_ROOT%\Scratch --config Release
-
-echo #################################
-echo ### Install binaries to system...
-echo #################################
 cmake --install %GIT_ROOT%\Scratch
+
+echo #########################################
+echo ### Install debug and release binaries...
+echo #########################################
+cmake --install %GIT_ROOT%\Scratch --config Debug
+cmake --install %GIT_ROOT%\Scratch --config Release
 
 pause
